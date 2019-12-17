ccbf12035efa8b7de59a48998eb1bf5ef428a380
Guillen Ruiz <I28480@verisk.com>
2019-12-17 18:01:42 +0100

    Fin de la seccion 6

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  equipo: any[] = [];
+
+  constructor( private http: HttpClient ) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo() {
+     //Leer archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe((resp: InfoPagina) => {
+
+      this.cargada = true;
+      this.info = resp;
+      console.log(resp);
+      // console.log(resp['twitter']);
+    });
+   }
+
+   private cargarEquipo(){
+       //Leer archivo JSON
+        this.http.get('https://angular-html-e1cb9.firebaseio.com/equipo.json')
+        .subscribe((resp: any []) => {
+
+          this.equipo = resp;
+          console.log(resp);
+          // console.log(resp['twitter']);
+        });
+      }
+
+}
